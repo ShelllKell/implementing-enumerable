50c17a9e77809f3dd782b4566ed1b51b17c8d89a
Shelby Kelly <shelby.kelly@colorado.edu>
2014-07-31 10:27:57 -0600

    All but last method

--- lib/gschool_enumerable.rb
+++ lib/gschool_enumerable.rb
@@ -1,3 +1,57 @@
 module GschoolEnumerable
 
+  def g_each(&block)
+    for item in self
+      block.call(item)
+    end
+    return self
+  end
+
+  def g_select
+    array = []
+    self.g_each do |item|
+      if yield(item)
+        array << item
+      end
+    end
+    array
+  end
+
+  def g_reject
+    array = []
+    self.g_each do |item|
+      if yield(item) == false
+        array << item
+      end
+    end
+    array
+  end
+
+
+  def g_map
+    array = []
+    self.g_each do |item|
+      array << yield(item)
+    end
+    array
+  end
+
+  def g_find
+    self.g_each do |item|
+      return item if yield(item)
+    end
+    nil
+  end
+
+
+  def g_select!
+    array = []
+    self.g_each do |item|
+      if yield(item) == false
+        item.g_reject
+      end
+    end
+    array
+  end
+
 end
